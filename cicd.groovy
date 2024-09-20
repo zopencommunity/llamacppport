d0dc64a30a7ec629d4cb8e3de307ed24a5ce312e
Igor Todorovski <itodorov@ca.ibm.com>
2024-09-20 17:22:16 -0400

    Change organization to zopencommunity

--- cicd.groovy
+++ cicd.groovy
@@ -6,9 +6,9 @@ node('linux')
       branches: [[name: '*/main']],
       doGenerateSubmoduleConfigurations: false,
       extensions: [],
-      userRemoteConfigs: [[url: 'https://github.com/ZOSOpenTools/llamacppport.git']]])
+      userRemoteConfigs: [[url: 'https://github.com/zopencommunity/llamacppport.git']]])
   }
   stage('Build') {
-    build job: 'Port-Pipeline', parameters: [string(name: 'PORT_GITHUB_REPO', value: 'https://github.com/ZOSOpenTools/llamacppport.git'), string(name: 'PORT_DESCRIPTION', value: "Port of Facebook's LLaMA model in C/C++" )]
+    build job: 'Port-Pipeline', parameters: [string(name: 'PORT_GITHUB_REPO', value: 'https://github.com/zopencommunity/llamacppport.git'), string(name: 'PORT_DESCRIPTION', value: "Port of Facebook's LLaMA model in C/C++" )]
   }
 }
