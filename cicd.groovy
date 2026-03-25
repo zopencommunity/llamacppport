8530a0b591a0156229b2cb6216a61c298441e7f0
Igor Todorovski <39890068+IgorTodorovskiIBM@users.noreply.github.com>
2026-03-25 16:01:41 -0400

    Change BUILD_LINE parameter to 'STABLE'
    Signed-off-by: Igor Todorovski <39890068+IgorTodorovskiIBM@users.noreply.github.com>

--- cicd.groovy
+++ cicd.groovy
@@ -9,6 +9,6 @@ node('linux')
       userRemoteConfigs: [[url: 'https://github.com/zopencommunity/llamacppport.git']]])
   }
   stage('Build') {
-    build job: 'Port-Pipeline', parameters: [string(name: 'PORT_GITHUB_REPO', value: 'https://github.com/zopencommunity/llamacppport.git'), string(name: 'PORT_DESCRIPTION', value: 'Enable AI inferencing on z/os' ), string(name: 'BUILD_LINE', value: 'DEV') ]
+    build job: 'Port-Pipeline', parameters: [string(name: 'PORT_GITHUB_REPO', value: 'https://github.com/zopencommunity/llamacppport.git'), string(name: 'PORT_DESCRIPTION', value: 'Enable AI inferencing on z/os' ), string(name: 'BUILD_LINE', value: 'STABLE') ]
   }
 }
